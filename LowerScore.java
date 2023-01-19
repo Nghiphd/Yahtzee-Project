8039761facccff1faddda86771e2fc4b69ab9357
Nghi Phan <nghi.phan@mrjaffesclass.com>
2023-01-19 10:42:33 -0800

    lowScore

---
+++ LowerScore.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class LowerScore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LowerScore
+{
+    public int lowScore;
+    
+    public LowerScore() {
+        
+    }
+    
+    public
+}
