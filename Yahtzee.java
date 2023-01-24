99268e274e080e24695b205795eebed02f962ab9
Nghi Phan <113135966+Nghiphd@users.noreply.github.com>
2023-01-24 13:58:55 -0800

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -97,7 +97,8 @@ public class Yahtzee
     }
     
     public String getScoreUpper() {
-        return "Score 1: " + scoreUpper[0]+" Score 2: " + scoreUpper[1]+" Score 3: " + scoreUpper[2]+" Score 4: " + scoreUpper[3]+" Score 5: " + scoreUpper[4]+" Score 6: " + scoreUpper[5];
+        return String.format(" Score 1: " + scoreUpper[0]+"\n Score 2: " + scoreUpper[1]+"\n Score 3: " + scoreUpper[2]+
+                             "\n Score 4: " + scoreUpper[3]+"\n Score 5: " + scoreUpper[4]+"\n Score 6: " + scoreUpper[5]);
     }
 }
 
